97dece2bfbd121a82df29079911d92191709c6f7
julianR <ju.romerol@duocuc.cl>
2026-05-05 16:07:45 -0400

    fead Inventario terminado

--- inventario-jj/src/main/java/com/inventario_jj/inventario_jj/InventarioJjApplication.java
+++ inventario-jj/src/main/java/com/inventario_jj/inventario_jj/InventarioJjApplication.java
@@ -2,10 +2,12 @@ package com.inventario_jj.inventario_jj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventarioJjApplication {
 
 	public static void main(String[] args) {
