25da8937ca506dd11b41dd497e49eec52255df6c
Jose Marcet <jo.marcet@duocuc.cl>
2026-05-05 17:11:36 -0400

    fead: microservicio pedido Incompleto

---
+++ lego-ms-orders-jj/src/main/java/com/orders_jj/lego_ms_orders_jj/LegoMsOrdersJjApplication.java
@@ -0,0 +1,13 @@
+package com.orders_jj.lego_ms_orders_jj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoMsOrdersJjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoMsOrdersJjApplication.class, args);
+	}
+
+}
---
+++ lego-ms-orders-jj/src/test/java/com/orders_jj/lego_ms_orders_jj/LegoMsOrdersJjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orders_jj.lego_ms_orders_jj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoMsOrdersJjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
