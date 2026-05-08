4569ee7c347b8b7742091ee5f019a6c0b1d8713a
julianR <ju.romerol@duocuc.cl>
2026-05-07 21:07:34 -0400

    Avance en controladores y servicios de LegoJJ

---
+++ catalogo-jj/src/main/java/com/catalogo_jj/config/DataConfig.java
@@ -0,0 +1,29 @@
+package com.catalogo_jj.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.catalogo_jj.repository.CatalogoRepository;
+import com.catalogo_jj.model.Lego;
+
+import java.util.List;
+
+@Configuration
+public class DataConfig {
+    @Bean
+    CommandLineRunner setupCatalogo(CatalogoRepository repository) {
+        return args -> {
+            //Solo insertaremos si la base de datos está vacía para evitar duplicados
+            if (repository.count() == 0) {
+                repository.saveAll(List.of(
+                        new Lego(null, "Halcón Milenario Star Wars", "Star Wars", 1351, 159990.0),
+                        new Lego(null, "Máquina de PAC-MAN Icons", "Icons", 2651, 249990.0),
+                        new Lego(null, "Auto de F1 McLaren Technic", "Technic", 1432, 189990.0),
+                        new Lego(null, "Castillo de Hogwarts Harry Potter", "Harry Potter", 6020, 449990.0),
+                        new Lego(null, "Skyline de Londres Architecture", "Architecture", 468, 39990.0)
+                ));
+                System.out.println(">>> Base de datos de Catálogo poblada con éxito.");
+            }
+        };
+    }
+}
---
+++ pago-jj2/src/main/java/com/pago_jj2/PagoJj2Application.java
@@ -0,0 +1,13 @@
+package com.pago_jj2.pago_jj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagoJj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagoJj2Application.class, args);
+	}
+
+}
